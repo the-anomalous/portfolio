e94e146e8c1d79ccdd722ebd56ff9221a57f31c4
Aditya Musale <adityamusale4477@gmail.com>
2022-07-28 22:11:12 +0530

    upadted appearOnScroll observer

--- src/observers/appearOnScroll.observer.js
+++ src/observers/appearOnScroll.observer.js
@@ -1,7 +1,7 @@
 const options = {
   root: null,
   threshold: 0,
-  rootMargin: '0px 0px -300px 0px'
+  rootMargin: '0px 0px -500px 0px'
 };
 
 const appearOnScroll = new IntersectionObserver((entries) => {
