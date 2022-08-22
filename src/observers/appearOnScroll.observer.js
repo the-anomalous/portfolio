9af363f03b747a72aeb47ad2f7aa13b79f47f187
Aditya Musale <adityamusale4477@gmail.com>
2022-08-22 23:58:29 +0530

    fixed minor bugs

--- src/observers/appearOnScroll.observer.js
+++ src/observers/appearOnScroll.observer.js
@@ -1,7 +1,7 @@
 const options = {
   root: null,
-  threshold: 0,
-  rootMargin: '0px 0px -200px 0px'
+  threshold: .2,
+  rootMargin: '0px 0px -75px 0px'
 };
 
 const appearOnScroll = new IntersectionObserver((entries) => {
